6dfca4226d0bb6db9018e633605b4b126f7f66dd
Busari Shakrudeen <busaribolaji08169324063@gmail.com>
2024-11-05 19:54:30 +0100

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 import { CitiesProvider } from "./contexts/CitiesContext";
 import Cities from "./component/Cities";
 import CityDetails from "./component/CityDetails";
 import CountryList from "./component/CountryList";
-import Form from "./component/form";
+import Form from "./component/Form";
 import { AuthenticateProvider } from "./contexts/AuthenticateContext";
 import ProtectedRoute from "./pages/ProtectedRoute";
 import { Suspense, lazy } from "react";
 import SpinnerFullPage from "./component/SpinnerFullPage";
 
 const HomePage = lazy(() => import("./pages/HomePage"));
-const PricingPage = lazy(() => import("./pages/PricingPage"));
-const ProductPage = lazy(() => import("./pages/ProductPage"));
+const PricingPage = lazy(() =>
+  import("./pages/PricingPage")
+);
+const ProductPage = lazy(() =>
+  import("./pages/ProductPage")
+);
 const LoginPage = lazy(() => import("./pages/LoginPage"));
-const AppLayout = lazy(() => import("./component/AppLayout"));
-const PageNotFound = lazy(() => import("./component/PageNotFound"));
+const AppLayout = lazy(() =>
+  import("./component/AppLayout")
+);
+const PageNotFound = lazy(() =>
+  import("./component/PageNotFound")
+);
 
 function App() {
   return (
@@ -25,8 +38,14 @@ function App() {
           <Suspense fallback={<SpinnerFullPage />}>
             <Routes>
               <Route index element={<HomePage />} />
-              <Route path="pricing" element={<PricingPage />} />
-              <Route path="product" element={<ProductPage />} />
+              <Route
+                path="pricing"
+                element={<PricingPage />}
+              />
+              <Route
+                path="product"
+                element={<ProductPage />}
+              />
               <Route path="login" element={<LoginPage />} />
               <Route
                 path="app"
@@ -36,10 +55,19 @@ function App() {
                   </ProtectedRoute>
                 }
               >
-                <Route index element={<Navigate to="cities" />} />
+                <Route
+                  index
+                  element={<Navigate to="cities" />}
+                />
                 <Route path="cities" element={<Cities />} />
-                <Route path="cities/:id" element={<CityDetails />} />
-                <Route path="countries" element={<CountryList />} />
+                <Route
+                  path="cities/:id"
+                  element={<CityDetails />}
+                />
+                <Route
+                  path="countries"
+                  element={<CountryList />}
+                />
                 <Route path="form" element={<Form />} />
               </Route>
               <Route path="*" element={<PageNotFound />} />
